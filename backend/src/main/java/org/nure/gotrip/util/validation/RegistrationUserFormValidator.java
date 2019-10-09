f4b9c81634236811fdbaf6652a76f17cd8253055
Anton Bulhakov <anton.bulhakov@nure.ua>
2019-10-10 00:10:18 +0300

    Added editing mechanism

--- backend/src/main/java/org/nure/gotrip/util/validation/RegistrationUserFormValidator.java
+++ backend/src/main/java/org/nure/gotrip/util/validation/RegistrationUserFormValidator.java
@@ -2,10 +2,13 @@ package org.nure.gotrip.util.validation;
 
 import org.nure.gotrip.exception.ValidationException;
 import org.nure.gotrip.dto.UserRegistrationFormDto;
+import org.nure.gotrip.model.RegisteredUser;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RegistrationUserFormValidator {
+
+    private static final int MAX_FULLNAME_SIZE = 50;
 
 	private static final String LOGIN_PATTERN = "[a-zA-Z0-9]{8,20}";
 
@@ -19,17 +22,50 @@ public class RegistrationUserFormValidator {
 			"(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-" +
 			"\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
 
-	public boolean registrationUserFormValid(UserRegistrationFormDto userRegistrationFormDto) throws ValidationException {
-		if (!userRegistrationFormDto.getLogin().matches(LOGIN_PATTERN)) {
-			throw new ValidationException("Invalid login");
-		}
-		if (!userRegistrationFormDto.getPassword().matches(PASS_PATTERN)) {
-			throw new ValidationException("Invalid password");
-		}
-		if (!userRegistrationFormDto.getEmail().matches(EMAIL_PATTERN)) {
-			throw new ValidationException("Invalid email");
-		}
+	private static final String FULL_NAME_PATTERN = "[a-zA-Z]{2,47} [a-zA-Z]{2,47}";
+
+    private static final String PHONE_PATTERN = "\\+[0-9]{15}";
+
+    public boolean registrationUserFormValid(UserRegistrationFormDto userRegistrationFormDto) throws ValidationException {
+		isRegistrationDataCorrect(
+		        userRegistrationFormDto.getLogin(),
+                userRegistrationFormDto.getPassword(),
+                userRegistrationFormDto.getEmail()
+        );
 		return true;
 	}
 
+	public void validateUser(RegisteredUser user) throws ValidationException{
+        isRegistrationDataCorrect(
+                user.getLogin(),
+                user.getPassword(),
+                user.getEmail()
+        );
+
+        isAdditionalDataCorrect(
+                user.getFullName(),
+                user.getPhone()
+        );
+    }
+
+    private void isRegistrationDataCorrect(String login, String password, String email) throws ValidationException{
+        if (!login.matches(LOGIN_PATTERN)) {
+            throw new ValidationException("Invalid login");
+        }
+        if (!password.matches(PASS_PATTERN)) {
+            throw new ValidationException("Invalid password");
+        }
+        if (!email.matches(EMAIL_PATTERN)) {
+            throw new ValidationException("Invalid email");
+        }
+    }
+
+    private void isAdditionalDataCorrect(String fullname, String phone) throws ValidationException{
+        if (!fullname.matches(FULL_NAME_PATTERN) && fullname.length() > MAX_FULLNAME_SIZE) {
+            throw new ValidationException("Invalid login");
+        }
+        if (!phone.matches(PHONE_PATTERN)) {
+            throw new ValidationException("Invalid password");
+        }
+    }
 }
