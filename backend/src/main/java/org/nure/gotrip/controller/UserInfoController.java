2663c4b4f28a9d4de38ecbb4d1e830d16cca0b02
Anton Bulhakov <anton.bulhakov@nure.ua>
2019-10-14 17:08:44 +0300

    Rewrite user info controller

--- backend/src/main/java/org/nure/gotrip/controller/UserInfoController.java
+++ backend/src/main/java/org/nure/gotrip/controller/UserInfoController.java
@@ -1,7 +1,6 @@
 package org.nure.gotrip.controller;
 
 import org.nure.gotrip.controller.response.NotFoundException;
-import org.nure.gotrip.dto.UserIdDto;
 import org.nure.gotrip.exception.NotFoundUserException;
 import org.nure.gotrip.model.RegisteredUser;
 import org.nure.gotrip.service.RegisteredUserService;
@@ -9,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,9 +24,9 @@ public class UserInfoController {
     }
 
     @GetMapping("/user/get")
-    public RegisteredUser getUserInfo(@RequestBody UserIdDto idDto){
+    public RegisteredUser getUserInfo(@RequestParam long userId){
         try {
-            return registeredUserService.findById(idDto.getId());
+            return registeredUserService.findById(userId);
         } catch (NotFoundUserException e) {
             logger.info(e.getMessage());
             throw new NotFoundException(e.getMessage());
