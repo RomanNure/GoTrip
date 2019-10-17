0a6fd69b602c5ab0d69c2145d7b9ad316feb368e
Vadym Bezkyshkyi <vadym.bezkyshkyi@nure.ua>
2019-10-18 00:40:31 +0300

    User page modified

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -16,10 +16,12 @@ export default class UserPage extends Component {
                     <div className="col-md-4">
                         <div className="panel panel-default">
                             <div className="panel-body text-center">
-                                <div className="pv-lg mr-3 ml-3"><img className="center-block img-circle img-responsive img-thumbnail rounded-circle thumb96 mt-3" src="images/Avatar.png" alt="Contact" /></div>
+                                <div className="pv-lg mr-3 ml-3"><img className="center-block img-circle img-responsive img-thumbnail rounded-circle thumb96" src="images/Avatar.png" alt="Contact" /></div>
                                 <h3 className="m0 text-bold">Audrey Hunt</h3>
-                                <div className="mv-lg">
-                                    <input ref="descr" value="description"/>
+                                <div className="row justify-content-center">
+                                    <div className="col-11">
+                                        <textarea class="form-control" id="exampleTextarea" placeholder="User description" row="4"></textarea>
+                                    </div>
                                 </div>
                                 <div className="text-center"><a className="btn btn-primary custom-btn mb-4 waves-effect #3abd94" href="">Send message</a></div>
                             </div>
@@ -44,37 +46,19 @@ export default class UserPage extends Component {
                                             <div className="form-group">
                                                 <label className="col-sm-2 control-label" htmlFor="inputContact2">Email</label>
                                                 <div className="col-md-10">
-                                                    <input id="inputContact2" type="email" placeholder="Name" defaultValue="" />
+                                                    <input id="inputContact2" type="email" placeholder="Email Address" defaultValue="" />
                                                 </div>
                                             </div>
                                             <div className="form-group">
                                                 <label className="col-sm-2 control-label" htmlFor="inputContact3">Phone</label>
                                                 <div className="col-md-10">
-                                                    <input id="inputContact3" type="text" defaultValue="" />
-                                                </div>
-                                            </div>
-                                            <div className="form-group">
-                                                <label className="col-sm-2 control-label" htmlFor="inputContact5">Website</label>
-                                                <div className="col-md-10">
-                                                    <input id="inputContact5" type="text" defaultValue="" />
+                                                    <input id="inputContact3" type="text" placeholder="Phone number" defaultValue="" />
                                                 </div>
                                             </div>
                                             <div className="form-group">
                                                 <label className="col-sm-2 control-label" htmlFor="inputContact6">Address</label>
                                                 <div className="col-md-10">
-                                                    <textarea className="materialize-textarea" id="inputContact6" defaultValue="lorem ipsum 69"row="4"/>
-                                                </div>
-                                            </div>
-                                            <div className="form-group">
-                                                <label className="col-sm-2 control-label" htmlFor="inputContact7">Social</label>
-                                                <div className="col-md-10">
-                                                    <input id="inputContact7" type="text" defaultValue="" />
-                                                </div>
-                                            </div>
-                                            <div className="form-group">
-                                                <label className="col-sm-2 control-label" htmlFor="inputContact8">Company</label>
-                                                <div className="col-md-10">
-                                                    <input id="inputContact8" type="text" placeholder="No Company" />
+                                                    <textarea className="materialize-textarea" id="inputContact6" placeholder="Address" defaultValue=""row="4"/>
                                                 </div>
                                             </div>
                                             <div className="form-group">
