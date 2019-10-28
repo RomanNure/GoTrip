00d7a8cc7efaa730cbbc583db8db5d2b2d3d5da4
Oleksii Makieiev1 <oleksii_makieiev1@epam.com>
2019-10-29 00:13:01 +0200

    [server] Refactored company classes

--- backend/src/main/java/org/nure/gotrip/util/validation/CompanyRegistrationValidator.java
+++ backend/src/main/java/org/nure/gotrip/util/validation/CompanyRegistrationValidator.java
@@ -17,10 +17,21 @@ public class CompanyRegistrationValidator {
 
 	private static final String NAME_PATTERN = "^[a-zA-Z]{2,47}$";
 
-	public boolean registrationCompanyValid(CompanyDto companyDto) throws ValidationException {
+	private static final String PHONE_NUMBER_PATTERN = "^(\\+*[0-9]{2}[.\\-\\s]?|00[.\\-\\s]?[0-9]{2}|0)([0-9]{1,3}[.\\-\\s]?(?:[0-9]{2}[.\\-\\s]?){4})$";
+
+	private static final String DOMAIN_PATTERN = "(^|\\s)((https?:\\/\\/)?[\\w-]+(\\.[a-z-]+)+\\.?(:\\d+)?(\\/\\S*)?)";
+
+	private static final String ADDRESS_PATTERN = "(.+)";
+
+	private static final String IMAGE_LINK_PATTERN = "(?!.*?\\/)(.*?)(?:\\.(?:jpg|jpeg|jpe|png|img))";
+
+	public void registrationCompanyValid(CompanyDto companyDto) throws ValidationException {
 		validateEmail(companyDto.getEmail());
 		validateName(companyDto.getName());
-		return true;
+		validatePhone(companyDto.getPhone());
+		validateDomain(companyDto.getDomain());
+		validateAddress(companyDto.getAddress());
+		validateImageLink(companyDto.getImageLink());
 	}
 
 	private void validateEmail(String email) throws ValidationException {
@@ -35,12 +46,52 @@ public class CompanyRegistrationValidator {
 		}
 	}
 
+	private void validatePhone(String phone) throws ValidationException {
+		if (isNotValidPhone(phone)) {
+			throw new ValidationException("Invalid phone");
+		}
+	}
+
+	private void validateDomain(String domain) throws ValidationException {
+		if (isNotValidDomain(domain)) {
+			throw new ValidationException("Invalid domain");
+		}
+	}
+
+	private void validateAddress(String address) throws ValidationException {
+		if (isNotValidAddress(address)) {
+			throw new ValidationException("Invalid address");
+		}
+	}
+
+	private void validateImageLink(String imageLink) throws ValidationException {
+		if (isNotValidImageLink(imageLink)) {
+			throw new ValidationException("Invalid imageLink");
+		}
+	}
+
 	private boolean isNotValidName(String name) {
 		return !name.matches(NAME_PATTERN);
 	}
 
 	private boolean isNotValidEmail(String email) {
 		return !email.matches(EMAIL_PATTERN);
+	}
+
+	private boolean isNotValidPhone(String phone) {
+		return !phone.matches(PHONE_NUMBER_PATTERN);
+	}
+
+	private boolean isNotValidDomain(String domain) {
+		return !domain.matches(DOMAIN_PATTERN);
+	}
+
+	private boolean isNotValidAddress(String address) {
+		return !address.matches(ADDRESS_PATTERN);
+	}
+
+	private boolean isNotValidImageLink(String imageLink) {
+		return !imageLink.matches(IMAGE_LINK_PATTERN);
 	}
 
 }
