a076427bead86141a63ffc64fb637e9cb5a8c3de
Roman <rk@pangnote.com>
2019-11-28 03:41:17 +0200

    -added abilitu to remove administrator

--- frontend/src/components/EmployeeList.js
+++ frontend/src/components/EmployeeList.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios'
-import ReactModal from 'react-modal';
-import { getCompanyAdmins } from '../api.js'
+import { ToastContainer, toast } from 'react-toastify';
+import { getCompanyAdmins, removeArdimistrator } from '../api.js'
 
 export default class EmployeeList extends Component {
     constructor(props) {
@@ -12,45 +10,20 @@ export default class EmployeeList extends Component {
         }
         this._getUsers()
     }
-    componentDidMount() {
-
-    }
 
     _getUsers = async () => {
-        /*  let options = {
-              method: "get",
-              url: 'https://go-trip.herokuapp.com/user/get?id=',
-              headers: {
-                  'Content-Type': 'application/x-www-from-urlencoded',//Content-Type': 'appication/json',
-              },
-          }
-  
-          let { administrators } = this.props
-          administrators.push({ id: 106 })
-          let ids = administrators.map(i => {
-              let url = JSON.parse(JSON.stringify(options))
-              url.url += i.id
-              return axios(url)
-          })
-          //console.log("ids=>",ids)
-          axios.all([ids])
-              .then(axios.spread((acct, perms) => {
-                  let users = []
-                  acct.map(i => {
-                      i.then(d => users.push(d.data))
-                  })
-                  console.log('users', users)
-  
-                  this.state.administrators = users
-                  this.setState()
-                  this.forceUpdate()
-              }));*/
         getCompanyAdmins(this.props.id).then(({ data }) => this.setState({ admins: data }))
     }
 
-    _onSentRequest = () => {
-        this.setState({ modal: false })
-        this.props._onAddAdmin(this.refs.admin.value)
+    _onRemoveAdmin = (id) => {
+        console.log("remove admin id=>", id)
+        removeArdimistrator(id)
+            .then(data => this._getUsers)
+            .catch(error => {
+                toast.error(error.response.data.message, {
+                    position: toast.POSITION.TOP_RIGHT
+                });
+            })
     }
 
     render() {
@@ -60,6 +33,8 @@ export default class EmployeeList extends Component {
         // console.log('administrators=>', administrators && administrators.length)
         return (
             <div className="container">
+                <ToastContainer />
+
                 <div className="row h3 m-4">
                     Company Employees
                 </div>
@@ -72,18 +47,18 @@ export default class EmployeeList extends Component {
                                     <div className="panel-title h5">Company Administrators</div>
                                 </div>
                                 {admins && admins.length > 0 ?
-                                    admins.map(({ id, login, fullName, email, avatarUrl }, i) => {
+                                    admins.map(({ id,administratorId, login, fullName, email, avatarUrl }, i) => {
                                         // console.log('admin=>', i, administrators[i])
-                                        return <div className="media" key={i} onClick={() => this.props.history.push('/user:' + id)}>
-                                            <div className="media-left mr-2">
+                                        return <div className="media" key={i} >
+                                            <div className="media-left mr-2" onClick={() => this.props.history.push('/user:' + id)}>
                                                 <a href="#"><img className="media-object rounded-circle img-thumbnail thumb48" src={avatarUrl ? avatarUrl : "images/Avatar.png"} alt="Contact" /></a>
                                             </div>
-                                            <div className="media-body pb-2">
+                                            <div className="media-body pb-2" onClick={() => this.props.history.push('/user:' + id)} >
                                                 <div className="text-bold">{fullName ? fullName : login}
                                                     <div className="text-sm text-muted">{email}</div>
                                                 </div>
                                             </div>
-                                            <i className="material-icons mt-2 mr-3 icon-red">highlight_off</i>
+                                            <i className="material-icons mt-2 mr-3 icon-red" style={{ cursor: "pointer" }} onClick={() => this._onRemoveAdmin(administratorId)}>highlight_off</i>
                                         </div>
                                     })
                                     :
